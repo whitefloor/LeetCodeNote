b65850a95998453e14e4678b8371980bca4cb5cb
JohnsonLiu <johnsonliu@sysage.com.tw-at-647350340558>
2023-03-27 18:27:46 +0800

    add log

--- Array/108. Convert Sorted Array to Binary Search Tree/main.go
+++ Array/108. Convert Sorted Array to Binary Search Tree/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "log"
+
 // LeetCode:https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
 // CookBook:https://books.halfrost.com/leetcode/ChapterFour/0100~0199/0108.Convert-Sorted-Array-to-Binary-Search-Tree/
 // Difficulty：Easy
@@ -25,9 +27,15 @@ func sortedArrayToBST(nums []int) *TreeNode {
 	if len(nums) == 0 {
 		return nil
 	}
+	log.Println(nums[len(nums)/2])
 	return &TreeNode{
 		Val:   nums[len(nums)/2],
 		Left:  sortedArrayToBST(nums[:len(nums)/2]),
 		Right: sortedArrayToBST(nums[len(nums)/2+1:]),
 	}
+}
+
+func main() {
+	tree := sortedArrayToBST([]int{-10, -3, 0, 5, 9})
+	log.Println(*tree.Right)
 }
