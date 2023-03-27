167866d881a8fabe5f1f1a574ac1b292631764ee
JohnsonLiu <johnsonliu@sysage.com.tw-at-647350340558>
2023-03-27 17:52:27 +0800

    #108

--- Array/108. Convert Sorted Array to Binary Search Tree/main.go
+++ Array/108. Convert Sorted Array to Binary Search Tree/main.go
@@ -5,3 +5,29 @@ package main
 // Difficulty：Easy
 // Time Complexity：O(n)
 // Space Complexity：O(n)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func sortedArrayToBST(nums []int) *TreeNode {
+	if len(nums) == 0 {
+		return nil
+	}
+	return &TreeNode{
+		Val:   nums[len(nums)/2],
+		Left:  sortedArrayToBST(nums[:len(nums)/2]),
+		Right: sortedArrayToBST(nums[len(nums)/2+1:]),
+	}
+}
