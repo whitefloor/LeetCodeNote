cbfbfde252d177fd235b6cea57f308dcf54ddd1b
JohnsonLiu <johnsonliu@sysage.com.tw-at-647350340558>
2023-05-23 14:08:43 +0800

    15

--- Array/15.3Sum/15.3Sum.go
+++ Array/15.3Sum/15.3Sum.go
@@ -13,11 +13,12 @@ import "sort"
 // 不過處理這種陣列重要的是最好先排序過會好處理很多
 // 而且這題是難在要處理重複解的部分，自己有寫出來計算的部分
 // 排序跟解決重複解的地方是上網看的
+// 有把參考到的解答稍微改寫一下，宣告的部分就比較簡單了
 
 func threeSum(nums []int) [][]int {
 	sort.Ints(nums)
-	result, start, end, index, length := make([][]int, 0), 0, 0, 0, len(nums)
-	for index = 1; index < length-1; index++ {
+	result, start, end, length := [][]int{}, 0, 0, len(nums)
+	for index := 1; index < length-1; index++ {
 		start, end = 0, length-1
 		if index > 1 && nums[index] == nums[index-1] {
 			start = index - 1
