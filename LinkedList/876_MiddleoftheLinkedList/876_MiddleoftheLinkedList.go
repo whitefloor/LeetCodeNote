3d5f22cf9b96668e378aa019232a4b9ed82437c3
whitefloor <a224412142009@hotmail.com>
2023-10-07 23:57:31 +0800

    876

---
+++ LinkedList/876_MiddleoftheLinkedList/876_MiddleoftheLinkedList.go
@@ -0,0 +1,26 @@
+package leetcode
+
+// LeetCode：https://leetcode.com/problems/middle-of-the-linked-list/description/
+// CookBook：解題的時候網站爆了，待補
+// Difficulty：Easy
+// Time Complexity：O(n)
+// Space Complexity：O(1)
+
+// note
+// 給你一個 linked list，找出在其中間的節點，如果中間正巧有兩個節點，則返回後面的那個
+// 蠻簡單的，Floyd演算法，一快一慢就可以解出來，沒看解答
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func middleNode(head *ListNode) *ListNode {
+	fast, slow := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+
+	return slow
+}
